6e2c41bced68796448a79947e2a6eeb9cddd2179
FabioLuizz <fabioluis19341@gmail.com>
2024-04-15 22:37:08 -0300

    feat: add entity

---
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,23 @@
+package com.example.crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "product")
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price_in_cents;
+
+}

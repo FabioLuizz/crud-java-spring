9cef48cf7970ccabb1b1775f4b8a878a4cb8e6da
FabioLuizz <fabioluis19341@gmail.com>
2024-04-15 23:04:45 -0300

    feat: added the post and put method

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,17 +1,50 @@
 package com.example.crud.controllers;
 
+import com.example.crud.domain.product.Product;
+import com.example.crud.domain.product.RequestProduct;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
+    @Autowired
+    private JpaRepository repository;
+
     @GetMapping
     public ResponseEntity getAllProducts() {
-        return ResponseEntity.ok().build();
+        var products = repository.findAll();
+        return ResponseEntity.ok(products);
+    }
+
+    @PostMapping
+    public ResponseEntity setProduct(@RequestBody @Valid RequestProduct data) {
+        Product product = new Product(data);
+        repository.save(product);
+        return ResponseEntity.ok("Produto cadastrado com sucesso!");
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data) {
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if(optionalProduct.isPresent()) {
+            var product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            repository.save(product);
+            return ResponseEntity.ok("Produto atualizado com sucesso");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
